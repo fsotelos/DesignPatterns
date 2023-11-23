28cc81c53e384e871fdd53c7e373034e13c9fb0b
Felipe Armando Sotelo Sotelo <felipe.sotelo@live.com>
2023-11-23 08:49:22 -0500

    Add Car and Car Builder

---
+++ demo/src/main/java/com/example/builder/Car.java
@@ -0,0 +1,33 @@
+package com.example.builder;
+
+public class Car {
+
+    public String make;
+    public String model;
+    public String color;
+    public int year;
+    public int horsePower;
+
+    public Car(){
+
+    }
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getHorsepower() {
+        return horsePower;
+    }
+} 
